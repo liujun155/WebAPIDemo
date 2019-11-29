147c0116d7ae587b5ba842cd3e205cad847c06fa
liujun <354277337@qq.com>
2019-11-29 16:07:09 +0800

    Swagger配置

---
+++ TestWebApi/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TestWebApi.Controllers
+{
+    public class UserController : ApiController
+    {
+        [HttpPost]
+        public string GetTestString()
+        {
+            return "HelloWorld";
+        }
+    }
+}
